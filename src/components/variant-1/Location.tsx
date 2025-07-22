87630391f24c60841d870ec2933e02d8c415d7ec
Ammad Rana <ammadrana862@gmail.com>
2025-07-22 18:33:50 +0500

    map updated

--- src/components/variant-1/Location.tsx
+++ src/components/variant-1/Location.tsx
@@ -7,10 +7,10 @@ const LocationMap = () => {
     <div className="w-full max-w-[1440px] mx-auto h-[500px] rounded-xl overflow-hidden mb-10 md:mb-14 lg:!mb-20">
       <Map
         height={500}
-        defaultCenter={[33.6366181, 72.9100046]} // J7 Mall location
+        defaultCenter={[33.661849634011745, 72.86950665018998]} // J7 Mall location
         defaultZoom={16}
       >
-        <Marker width={50} anchor={[33.6366181, 72.9100046]} />
+        <Marker width={50} anchor={[33.661849634011745, 72.86950665018998]} />
       </Map>
     </div>
   );
